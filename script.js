954952f5d112777724758ac91f2d1b4295a9207c
Ivan Peresvetov <lumenist@mail.ru>
2023-12-03 23:12:23 +0300

    0312

---
+++ script.js
@@ -0,0 +1,51 @@
+let fiveRepos
+
+const debounce = (fn, debounceTime) => {
+    let bounces = 0
+    return function(...args) {
+        let newF = fn.bind(this)
+        bounces++
+        setTimeout(function(){
+            if (bounces === 1) newF(...args)
+            bounces--
+        }, debounceTime)
+    }
+};
+
+async function searchRepo(searchStr) {
+    let searchResult = await fetch(`https://api.github.com/search/repositories?q=${searchStr}`)
+    if (searchResult.ok) {
+        const resultProm = searchResult.json()
+        console.log(resultProm)
+        resultProm.then((result) => {
+            console.log(result.items.slice(0, 5))
+            fiveRepos = result.items.slice(0, 5)
+        })
+    } else {
+        console.log(`Ошибка HTTP: ${searchResult.status}`)
+    }
+}
+
+const searchRepoDebounce = debounce(searchRepo, 1000)
+const searchInput = document.querySelector('.search__input')
+searchInput.addEventListener('input', function(e) {
+    searchRepoDebounce(e.target.value)
+})
+
+const testButton = document.querySelector('button')
+testButton.addEventListener('click', function(e) {
+    addRepo(fiveRepos[0])
+})
+
+function addRepo(repo) {
+    let pinnedRepo = document.querySelector('.search__template').content
+    let name = pinnedRepo.querySelector('.pinnedrepo__name')
+    let owner = pinnedRepo.querySelector('.pinnedrepo__owner')
+    let stars = pinnedRepo.querySelector('.pinnedrepo__stars')
+    name.textContent = `Name: ${repo.name}`
+    owner.textContent = `Owner: ${repo.owner.login}`
+    stars.textContent = `Stars: ${repo.stargazers_count}`    
+    
+    let listOfRepost = document.querySelector('.search__pins')
+    listOfRepost.append(pinnedRepo)
+}
