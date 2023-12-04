b8534cdc6479a88e96de3e7b792c11496fc8f3f1
Ivan Peresvetov <lumenist@mail.ru>
2023-12-04 21:19:54 +0300

    0412 to home

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// нужно сокращение имени, если имя слишком большое
-// автокомплит сначала скрыт
-// если набрано что-то, то он появляется
+// нужно сокращение имени в списке и пине, если имя слишком большое
+// скрытие списка, если ничего не введено - отловить баг того что что-то появляется
+//// появляется если добавить символ и сразу удалить
+//// надо сделать что бы если в поиске пусто - поиск не происходил
+// запретить ввод пробела
+//// сообщение о пробеле
+// сообщение о проблеме с сетью при её отсутствии
 
 
-let fiveRepos
+// удаление из пина по нажатию на крест
+
 
 const debounce = (fn, debounceTime) => {
     let bounces = 0
@@ -19,47 +24,60 @@ const debounce = (fn, debounceTime) => {
 
 async function searchRepo(searchStr) {
     let searchResult = await fetch(`https://api.github.com/search/repositories?q=${searchStr}`)
+    const searchInput = document.querySelector('input')
     if (searchResult.ok) {
         let autoComlite = document.querySelector('.search__autocomplite')
         const resultProm = searchResult.json()
-        console.log(resultProm)
+        autoComlite.replaceChildren()
         resultProm.then((result) => {
-            result.items.slice(0, 5).forEach(function(repo) {
+            result.items.slice(0, 5).forEach(function(repo) {                
                 let newLi = document.createElement('li')
                 newLi.textContent = repo.name
                 newLi.classList.add('search__result')
+                newLi.dataset.name = repo.name
+                newLi.dataset.owner = repo.owner.login
+                newLi.dataset.stars = repo.stargazers_count
                 autoComlite.appendChild(newLi)
-                console.log(repo.name)
             })
-            // console.log(result.items.slice(0, 5))
-            // fiveRepos = result.items.slice(0, 5)
         })
     } else {
         console.log(`Ошибка HTTP: ${searchResult.status}`)
     }
 }
 
-const searchRepoDebounce = debounce(searchRepo, 1000)
+const searchRepoDebounce = debounce(searchRepo, 400)
+
 const searchInput = document.querySelector('.search__input')
-
 searchInput.addEventListener('input', function(e) {
-    searchRepoDebounce(e.target.value)    
+    let autoComlite = document.querySelector('.search__autocomplite')
+    if (e.target.value === '') {
+        autoComlite.replaceChildren()
+    } else {
+        searchRepoDebounce(e.target.value)
+    }
 })
 
-const testButton = document.querySelector('button')
-testButton.addEventListener('click', function(e) {
-    addRepo(fiveRepos[0])
-})
-
-function addRepo(repo) {
-    let pinnedRepo = document.querySelector('.search__template').content
+function addRepo(repName, repOwner, repStars) {
+    let pinnedRepo = document.querySelector('.search__template').content.cloneNode(true)
     let name = pinnedRepo.querySelector('.pinnedrepo__name')
     let owner = pinnedRepo.querySelector('.pinnedrepo__owner')
     let stars = pinnedRepo.querySelector('.pinnedrepo__stars')
-    name.textContent = `Name: ${repo.name}`
-    owner.textContent = `Owner: ${repo.owner.login}`
-    stars.textContent = `Stars: ${repo.stargazers_count}`    
-    
+    name.textContent = `Name: ${repName}`
+    owner.textContent = `Owner: ${repOwner}`
+    stars.textContent = `Stars: ${repStars}`    
     let listOfRepost = document.querySelector('.search__pins')
     listOfRepost.append(pinnedRepo)
 }
+
+const autoComliteZone = document.querySelector('.search__autocomplite')
+autoComliteZone.addEventListener('click', function(e) {
+    addRepo(e.target.dataset.name, e.target.dataset.owner, e.target.dataset.stars)
+})
+
+const searchPins = document.querySelector('.search__pins')
+searchPins.addEventListener('click', function (e) {
+    if (e.target.classList.contains('pinnedrepo__deleterepo')) {
+        console.log(e.target)
+        console.log(e.target.closest('.pinnedrepo'))
+    }
+})
